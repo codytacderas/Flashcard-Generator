96d20849d5b9d6871e3e8d237423d281729a19b0
Cody Tacderas <codytacderas@Codys-MacBook-Air.local>
2017-07-29 18:21:45 -0600

    With recursion

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,13 +1,6 @@
-var ClozeCard = function(question, answer) {
-  this.question = question;
-  this.answer = answer;
-  
-  // this.getWeather = function() {
-  //   weather.find({ search: this.location, degreeType: "F" }, function(err, result) {
-  //     if (err) console.log(err);
-  //     console.log(JSON.stringify(result, null, 2));
-  //   });
-  // };
+var ClozeCard = function(text, cloze) {
+  this.text = text;
+  this.cloze = cloze;
 };
 
 module.exports = ClozeCard;
