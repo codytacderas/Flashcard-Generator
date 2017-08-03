463a7662113497158f7b888a3466c5d9b42e6dc8
Cody Tacderas <codytacderas@Codys-MacBook-Air.local>
2017-08-02 20:03:38 -0600

    Still slightly broken.

--- flashcards.js
+++ flashcards.js
@@ -1,7 +1,7 @@
 // Require for BasicCard export
-var basicCard = require("BasicCard");
+var basicCard = require('BasicCard');
 // Require for ClozeCard export
-var clozeCard = require("ClozeCard");
+var clozeCard = require('ClozeCard');
 // Require for NPM inquirer
 var inquirer = require('inquirer');
 // Require for file system
@@ -12,7 +12,6 @@ var newCards = [];
 var count = 0;
 
 var beginReview = function() {
-	if (count <= 100) {
 		// Inquirer prompt that prompts for user input
 		inquirer.prompt ({
 				name: 'flashcard',
@@ -24,8 +23,8 @@ var beginReview = function() {
 			// If the response is create, inquire which kind of card to create
 			if (response.flashcard == 'Create') {
 				inquirer.prompt ({	
-						name: "typeOfCard",
-						type: "list",
+						name: 'typeOfCard',
+						type: 'list"'
 						message: 'What kind of card would you like to create?',
 						choices: ['Basic Card', 'Cloze Card']
 					})
@@ -34,21 +33,43 @@ var beginReview = function() {
 					// Inquire for card input--the front then the back of card.
 
 					if (response.typeOfCard == 'Basic Card') {
-						inquire.prompt ([{
+						inquire.prompt ([
+						{
 							name: 'frontCard',
 							type: 'input',
 							message: 'Please write the front of your flash card.'
 						},
 						{
-							name: "backCard",
-							type: "input",
-							message:"Write the back of your Flash Card."
+							name: 'backCard',
+							type: 'input',
+							message: 'Please write the back of your flash card..'
 						}
 						])
-						.then(function(answers){
+						.then (function(response) {
 							var basic = new basicCard(answers.front, answers.back);
 							console.log(basic.front);
-							fs.appendFile("BasicCards.txt", JSON.stringify(basic+"\n"), function (err) {
+							fs.appendFile('BasicCards.txt"' JSON.stringify(basic + '\n'), function (err) {
+							  if (err) throw err;
+							  console.log('Your card has been saved!');
+							})
+						})
+					}
+					else if (response.typeOfCard == 'Cloze Card') {
+						inquire.prompt ([{
+							name:'text',
+							type: 'input',
+							message: 'Please write the full text of your flashcard.'
+						},
+						{
+							name: 'cloze',
+							type: 'input',
+							message: 'Please write the redacted version of your flashcard.'
+						}
+						])
+						.then(function(answers) {
+							var cloze = new clozeCard(answers.text, answers.cloze);
+							console.log(cloze.fullText);
+							fs.appendFile('ClozeCards.txt', JSON.stringify(cloze)+'\n', function (err) {
 							  if (err) throw err;
 							  console.log('Your card has been saved!');
 							})
